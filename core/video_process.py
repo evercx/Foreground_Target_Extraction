e22ed093fb7cdbe4309bc4ce1979ebc84dbab0ef
evercx <306534137@qq.com>
2017-09-17 17:55:10 +0800

    init

--- core/video_process.py
+++ core/video_process.py
@@ -7,39 +7,59 @@ import sys
 
 class App:
 
-    def __init__(self,videoSrc,bgs):
-        self.bgs = bgs
+    def __init__(self,videoSrc,bgsName):
+
         self.videoSrc = videoSrc
-        print("视频地址:",videoSrc)
+        self.bgsName = bgsName
+        #print("视频地址:",videoSrc)
 
     def run(self):
         capture = cv2.VideoCapture(self.videoSrc)
+        pic = capture.read()
+        print(type(pic))
+        print(pic)
         while not capture.isOpened():
             capture = cv2.VideoCapture(self.videoSrc)
             cv2.waitKey(1000)
             print("Wait for the header")
 
+        if self.bgsName == "DPMean":
+            bgs = libbgs.DPMean()
+        elif self.bgsName == "LBP_MRF":
+            bgs = libbgs.LBP_MRF()
+        elif self.bgsName == "MultiLayer":
+            bgs = libbgs.MultiLayer()
+
         while True:
             flag, frame = capture.read()
+            #print(frame)
+            img_output = None
+            img_bgmodel = None
             if flag:
-                img_output = self.bgs.apply(frame)
-                img_bgmodel = self.bgs.getBackgroundModel();
+                img_output = bgs.apply(frame)
+                img_bgmodel = bgs.getBackgroundModel();
 
                 cv2.imshow('img_raw', frame)
                 cv2.imshow('img_output', img_output)
                 cv2.imshow('img_bgmodel', img_bgmodel)
 
+
                 cv2.moveWindow('img_raw', 100, 100)
                 cv2.moveWindow('img_output', 100, 300)
                 cv2.moveWindow('img_bgmodel', 100, 500)
             else:
-                cv2.waitKey(2000)
+                cv2.waitKey(1500)
+                cv2.destroyAllWindows()
                 break
             if 0xFF & cv2.waitKey(10) == 27:
+                cv2.destroyAllWindows()
                 break
 
-        cv2.waitKey(5000)
+        cv2.waitKey(1500)
         cv2.destroyAllWindows()
 
 
 
+if __name__ == '__main__':
+
+    App('../dataset/NoShake_StaticBg/airport.avi',"DPMean").run()
--- run.py
+++ run.py
@@ -5,6 +5,9 @@ import cv2
 import libbgs
 import sys
 
+import core.video_process as video
+
+
 
 
 # bgs = libbgs.DPMean()?
@@ -14,38 +17,85 @@ import sys
 
 def choose_algorithms(inputMessage):
 
-    # algorithms = []
-    # algorithms.append(libbgs.DPMean())
-    # algorithms.append(libbgs.LBP_MRF())
-    # algorithms.append(libbgs.MultiLayer())
-
-
     if inputMessage == "0":
         print("你选择的是算法0:DPMean")
-        return libbgs.DPMean()
+        return "DPMean"
     elif inputMessage == "1":
         print("你选择的是算法1:LBP_MRF")
-        return libbgs.LBP_MRF()
+        return "LBP_MRF"
     elif inputMessage == "2":
         print("你选择的是算法2:MultiLayer")
-        return libbgs.MultiLayer()
+        return "MultiLayer"
     elif inputMessage == "q":
         sys.exit(0)
     else:
         print("你输入的字符无效")
+        return None
+
+def choose_video(inputMessage):
+
+    if inputMessage == "0":
+        print("你选择的是视频0:不晃动的动态背景的水流")
+        return "./dataset/NoShake_MoveBg/waterSurface.avi"
+    elif inputMessage == "1":
+        print("你选择的是视频1:不晃动的静态背景的机场")
+        return "./dataset/NoShake_StaticBg/airport.avi"
+    elif inputMessage == "2":
+        print("你选择的是视频2:不晃动的静态背景的大厅")
+        return "./dataset/NoShake_StaticBg/hall.avi"
+    elif inputMessage == "3":
+        print("你选择的是视频3:不晃动的静态背景的办公室")
+        return "./dataset/NoShake_StaticBg/office.avi"
+    elif inputMessage == "4":
+        print("你选择的是视频4:不晃动的静态背景的行人")
+        return "./dataset/NoShake_StaticBg/pedestrian.avi"
+    elif inputMessage == "5":
+        print("你选择的是视频5:不晃动的静态背景的烟")
+        return "./dataset/NoShake_StaticBg/smoke.avi"
+    elif inputMessage == "6":
+        print("你选择的是视频6:晃动的车6")
+        return "./dataset/Shake/cars6.avi"
+    elif inputMessage == "7":
+        print("你选择的是视频7:晃动的车7")
+        return "./dataset/Shake/cars7.avi"
+    elif inputMessage == "8":
+        print("你选择的是视频8:晃动的人1")
+        return "./dataset/Shake/people1.avi"
+    elif inputMessage == "9":
+        print("你选择的是视频9:晃动的人2")
+        return "./dataset/Shake/people2.avi"
+    elif inputMessage == "q":
+        sys.exit(0)
+    else:
+        print("你输入的字符无效")
+        return None
 
 def main():
-
-
 
 
     while True:
         print("前景目标提取程序\n")
         print("0:DPMean\t\t 1:LBP_MRF\t\t 2:MultiLayer \t\tq:退出本程序")
         inputMessage = raw_input("请选择算法: ");
-        choose_algorithms(inputMessage)
+        algorithms = choose_algorithms(inputMessage)
+        print("\n0:不晃动的动态背景的水流")
+        print("1:不晃动的静态背景的机场")
+        print("2:不晃动的静态背景的大厅")
+        print("3:不晃动的静态背景的办公室")
+        print("4:不晃动的静态背景的行人")
+        print("5:不晃动的静态背景的烟")
+        print("6:晃动的车6")
+        print("7:晃动的车7")
+        print("8:晃动的人1")
+        print("9:晃动的人2")
+        print("q:退出本程序")
+        inputMessage = raw_input("请选择视频: ");
+        videoSrc = choose_video(inputMessage)
 
-
+        if videoSrc != None and algorithms != None:
+            print("播放视频...")
+            video.App(videoSrc,algorithms).run()
+            print("视频播放完毕...")
 
 if __name__ == '__main__':
     main()
